8e47dac680d10eb9d9afe7a940f0e7675763caff
sriramwithgithub <sriramhadoop27@gmail.com>
2024-09-04 19:12:59 +0530

    add script.sh

---
+++ script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Database connection string
+DATABASE_URL=$(heroku config:get DATABASE_URL -a your-app-name)
+
+# Interval between data sends (in seconds)
+INTERVAL=5
+
+# Function to insert data into the database
+send_data() {
+    local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    local random_value=$((RANDOM % 100))
+
+    # SQL command to insert data
+    local sql="INSERT INTO data_table (timestamp, value) VALUES ('$timestamp', $random_value);"
+
+    # Execute the SQL command
+    echo "$sql" | psql $DATABASE_URL
+}
+
+# Loop to continuously send data
+while true; do
+    send_data
+    echo "Data sent at $(date)"
+    sleep $INTERVAL
+done
